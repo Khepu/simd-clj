563e1a62dc263430025224cee7104b7c1eeb5345
Georgios Makris <makris.giwrgos1234@gmail.com>
2024-06-23 21:30:23 +0200

    Simplify generation of symbols

--- src/simd_clj/core.clj
+++ src/simd_clj/core.clj
@@ -5,28 +5,26 @@
 
 (set! *warn-on-reflection* true)
 
-(def ^:const vector-types '(ByteVector ShortVector IntVector))
+(def ^:const vector-types '("Byte" "Short" "Int" "Long" "Float" "Double"))
 
 (def ^:const bit-sizes [64 128 256 512])
 
 (defn create-species-kw [class-name size]
-  (let [name (-> class-name
-                 name
-                 (s/split #"Vector")
-                 first
-                 s/lower-case)]
+  (let [name (s/lower-case class-name)]
     (keyword (str name "-" size))))
 
 (defn create-species-enum [class-name size]
-  (symbol (str "jdk.incubator.vector." (name class-name)) (str "SPECIES_" size)))
+  (symbol (str "jdk.incubator.vector." (str (name class-name) "Vector"))
+          (str "SPECIES_" size)))
 
 (defn create-species-fn [class-name fn-name]
-  (symbol (str "jdk.incubator.vector." (name class-name)) (name fn-name)))
+  (symbol (str "jdk.incubator.vector." (str (name class-name) "Vector"))
+          (name fn-name)))
 
 (defn call-defop [name args vector-type size]
-  (let [species-kw (create-species-kw vector-type size)
+  (let [species-kw   (create-species-kw   vector-type size)
         species-enum (create-species-enum vector-type size)
-        body (create-species-fn vector-type name)]
+        body         (create-species-fn   vector-type name)]
     `(defmethod ~name ~species-kw [~@args ~'type]
        (~body ~species-enum ~@args))))
 
@@ -39,7 +37,7 @@
   `(do
      ~(call-defop-multi name args)
      ~@(for [vector-type vector-types
-             size bit-sizes]
+             size        bit-sizes]
          (call-defop name args vector-type size))))
 
 (defop broadcast [value])
